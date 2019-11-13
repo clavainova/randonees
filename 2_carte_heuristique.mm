495e27ecea2fc5a6f7842d2b708d08cedeeb2e94
clavainova <clavainova@gmail.com>
2019-11-13 15:35:18 +0100

    jour 2

---
+++ 2_carte_heuristique.mm
@@ -0,0 +1,80 @@
+<map version="1.0.1">
+    <node ID="ID_1">
+        <richcontent TYPE="NODE">
+            <html>
+                <head/>
+                <body>
+                    <p>Randonn&#233;es en Ossau</p>
+                </body>
+            </html>
+        </richcontent>
+        <node ID="ID_20" POSITION="left" STYLE="fork" TEXT="Client">
+            <node ID="ID_23" POSITION="left" STYLE="fork" TEXT="la Communaut&#233; des Communes "/>
+            <node ID="ID_26" POSITION="left" STYLE="fork" TEXT="les d&#233;cideurs de la CCVO "/>
+            <node ID="ID_24" POSITION="left" STYLE="fork" TEXT="l&#8217;office du tourisme de la vall&#233;e"/>
+            <node ID="ID_25" POSITION="left" STYLE="fork" TEXT="l&#8217;&#233;cole local"/>
+        </node>
+        <node ID="ID_8" POSITION="right" STYLE="fork" TEXT="Objectifs">
+            <node ID="ID_9" POSITION="right" STYLE="fork" TEXT="aider la communatu&#233;">
+                <node ID="ID_59" POSITION="right" STYLE="fork" TEXT="Signaler les probl&#232;mes ">
+                    <node ID="ID_19" POSITION="right" STYLE="fork" TEXT="affaissements"/>
+                    <node ID="ID_13" POSITION="right" STYLE="fork" TEXT="arbre tomb&#233;"/>
+                    <node ID="ID_18" POSITION="right" STYLE="fork" TEXT="&#233;coulements d'eau"/>
+                </node>
+                <node ID="ID_72" POSITION="right" STYLE="fork" TEXT="Plus de tourisme"/>
+            </node>
+            <node ID="ID_38" POSITION="right" STYLE="fork" TEXT="Aider les ranndoneurs">
+                <node ID="ID_29" POSITION="right" STYLE="fork" TEXT="randonn&#233;es">
+                    <node ID="ID_39" POSITION="right" STYLE="fork" TEXT="GPX fichiers">
+                        <node ID="ID_51" POSITION="right" STYLE="fork" TEXT="la visualisation et la finesse de la localisation"/>
+                    </node>
+                    <node ID="ID_40" POSITION="right" STYLE="fork" TEXT="PDF cartes">
+                        <node ID="ID_33" POSITION="right" STYLE="fork" TEXT="Circuits VTT"/>
+                        <node ID="ID_32" POSITION="right" STYLE="fork" TEXT="Circuits p&#233;destrares"/>
+                    </node>
+                    <node ID="ID_47" POSITION="right" STYLE="fork" TEXT="type de randonn&#233;es">
+                        <node ID="ID_48" POSITION="right" STYLE="fork" TEXT="p&#233;destre">
+                            <node ID="ID_83" POSITION="right" STYLE="fork" TEXT="difficult&#233;s/longeur"/>
+                            <node ID="ID_84" POSITION="right" STYLE="fork" TEXT="temps de parcours"/>
+                            <node ID="ID_85" POSITION="right" STYLE="fork" TEXT="distance"/>
+                            <node ID="ID_86" POSITION="right" STYLE="fork" TEXT="d&#233;nivel&#233;"/>
+                        </node>
+                        <node ID="ID_49" POSITION="right" STYLE="fork" TEXT="canoyoning"/>
+                        <node ID="ID_50" POSITION="right" STYLE="fork" TEXT="VTT"/>
+                    </node>
+                    <node ID="ID_87" POSITION="right" STYLE="fork" TEXT="Pr&#233;parer sa randonn&#233;e">
+                        <node ID="ID_88" POSITION="right" STYLE="fork" TEXT="M&#233;teo"/>
+                        <node ID="ID_89" POSITION="right" STYLE="fork" TEXT="&#233;quipement"/>
+                    </node>
+                    <node ID="ID_41" POSITION="right" STYLE="fork" TEXT="Niveau de difficult&#233;">
+                        <node ID="ID_42" POSITION="right" STYLE="fork" TEXT="familiale"/>
+                        <node ID="ID_43" POSITION="right" STYLE="fork" TEXT="sportive"/>
+                        <node ID="ID_44" POSITION="right" STYLE="fork" TEXT="avertis"/>
+                    </node>
+                </node>
+                <node ID="ID_12" POSITION="right" STYLE="fork" TEXT="obtenir aide dans une urgence">
+                    <node ID="ID_14" POSITION="right" STYLE="fork" TEXT="urgence medicale"/>
+                    <node ID="ID_15" POSITION="right" STYLE="fork" TEXT="personnes qui sont perdus"/>
+                </node>
+            </node>
+        </node>
+        <node ID="ID_63" POSITION="left" STYLE="fork" TEXT="Exigences de appli">
+            <node ID="ID_64" POSITION="left" STYLE="fork" TEXT="idetifier et suivre des randonn&#233;es directement avec le GPS "/>
+            <node ID="ID_65" POSITION="left" STYLE="fork" TEXT="donner les cartes pertinentes et on peut rechercher pour les cartes specifiques"/>
+            <node ID="ID_90" POSITION="left" STYLE="fork" TEXT="appli et syst&#232;me des remont&#233;es d'informations "/>
+            <node ID="ID_66" POSITION="left" STYLE="fork" TEXT="communiquer les messages dans le deux sens"/>
+            <node ID="ID_67" POSITION="left" STYLE="fork" TEXT="Garder les donn&#233;es personnels"/>
+        </node>
+        <node ID="ID_74" POSITION="left" STYLE="fork" TEXT="Des grandes &#233;tapes">
+            <node ID="ID_77" POSITION="left" STYLE="fork" TEXT="Maquettage"/>
+            <node ID="ID_80" POSITION="left" STYLE="fork" TEXT="Aspects algorithmiques"/>
+            <node ID="ID_81" POSITION="left" STYLE="fork" TEXT="Bases de donn&#233;es"/>
+            <node ID="ID_82" POSITION="left" STYLE="fork" TEXT="Application mobile"/>
+        </node>
+        <node ID="ID_52" POSITION="right" STYLE="fork" TEXT="Securit&#233;">
+            <node ID="ID_57" POSITION="right" STYLE="fork" TEXT="Les droits sur les donn&#233;es num&#233;riques"/>
+            <node ID="ID_58" POSITION="right" STYLE="fork" TEXT="la loi RGPD  pour les donn&#233;es personelles"/>
+            <node ID="ID_73" POSITION="right" STYLE="fork" TEXT="La r&#233;cup&#233;ration et reutilisation des donn&#233;es"/>
+        </node>
+    </node>
+</map>
